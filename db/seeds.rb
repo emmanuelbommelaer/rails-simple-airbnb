1886908edccbf1f5c8d49224a663a9472092fb5d
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-18 20:31:28 +0100

    Flat model and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Starting fake data generation"
+10.times do
+  Flat.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence(word_count: 10),
+    price_per_night: (50..500).to_a.sample,
+    number_of_guests: (2..16).to_a.sample
+  )
+  puts "flat generated"
+end
+puts "ok"
