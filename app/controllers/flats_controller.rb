afce6f5214279a7a12fcd21234d6c598077298b8
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-20 09:12:42 +0100

    ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,14 @@
 class FlatsController < ApplicationController
+  def index
+    if params[:query] != ""
+      raise
+      @flats = Flat.where("name LIKE '%#{@query}%'")
+    else
+      @flats = Flat.all
+    end
+  end
+
+  def new
+    @flat = Flat.new
+  end
 end
